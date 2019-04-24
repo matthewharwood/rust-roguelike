df6fa020ee6baf8b328508544350286e3c9eb106
Matthew Harwood <matthhar12@gmail.com>
2019-04-23 21:24:28 -0700

    added moving @ symbol.

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,66 @@ const LIMIT_FPS: i32 = 20;
 fn main() {
     tcod::system::set_fps(LIMIT_FPS);
 
+
     let mut root: Root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
         .font_type(FontType::Greyscale)
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcode Tutorial")
         .init();
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT /2;
 
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
         root.clear();
-        root.put_char(1,1,'@',BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
 
+        // handle keys and exit game if needed
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+
 }
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+    let key = root.wait_for_keypress(true);
+    match key {
+        Key { code: Enter, alt: true, .. } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen = root.is_fullscreen();
+            root.set_fullscreen(!fullscreen);
+        }
+        Key { code: Escape, .. } => return true, // exit game
+
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {}
+    }
+
+
+    false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
